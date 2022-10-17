75bb9dd1d9b47a363cb53fb862549f3fb63d5866
Luiz Antonio Anacleto Zuchinali <luizanacletozuchinali@gmail.com>
2022-10-17 09:48:30 -0300

    Update gitignore

---
+++ src/Carutiq.App/Program.cs
@@ -0,0 +1,21 @@
+﻿Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+
+//TODO: Create a build to generate application and configure services
+var services = new ServiceCollection();
+
+services.AddSingleton<BrokerConfiguration>(BrokerConfiguration.GetConfig());
+services.AddScoped<IConnectionHandler, ConnectionHandler>();
+
+var provider = services.BuildServiceProvider();
+
+var server = ActivatorUtilities.CreateInstance<ApplicationServer>(provider);
+
+var tokenSrc = new CancellationTokenSource();
+var token = tokenSrc.Token;
+
+var config = provider.GetRequiredService<BrokerConfiguration>();
+await server.Listen(() => { Log.Information("Running on {Ip}:{Port}", config.Host, config.Port); }, token);
---
+++ src/Carutiq.App/usings.cs
@@ -0,0 +1,4 @@
+﻿global using Carutiq.Core;
+global using Carutiq.Server;
+global using Serilog;
+global using Microsoft.Extensions.DependencyInjection;
